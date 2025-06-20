01b1ca261cc43921b209fced66825ec552254797
Sofie Tessem Wang <sofietessemwang@gmail.com>
2025-06-20 14:40:31 +0200

    small color change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar shadow-sm">
+    <div className="navbar shadow-sm text-customprimary">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
