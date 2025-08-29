e10ccb0034e8922975458daa311af9119b415540
Sofie Tessem Wang <sofiewang@dhcp-10-24-67-29.wlan.ntnu.no>
2025-08-29 10:09:32 +0200

    about page updated

---
+++ app/about/page.tsx
@@ -0,0 +1,60 @@
+import Education from "@/components/about/Education";
+import Header from "@/components/Header";
+import Hero from "@/components/about/Hero";
+import Footer from "@/components/Footer";
+import ContactForm from "@/components/form";
+import Navbar from "@/components/Navbar";
+import { EmailFormProvider } from "@/context/EmailFormContext";
+import React from "react";
+import Motivation from "@/components/about/Motivation";
+import Timeline from "@/components/about/TimelineVertical";
+import TimelineHorizontal from "@/components/about/TimelineHorizontal";
+
+const AboutPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <EmailFormProvider>
+        <ContactForm />
+        <Navbar />
+        <main className="flex-1 ">
+          {/* header */}
+          {/* <div className="min-h-80 bg-[#a7c6ed] content-center">
+            <h1 className="text-center">Om meg</h1>
+          </div> */}
+          <Header title={"Om meg"} subtitle="Hjem > Om" />
+          <Hero />
+          {/* <Education/> */}
+          <Motivation />
+
+          <div className="py-10 ">
+            <h2 className="text-center">
+              Hva jeg har holdt på med i det siste
+            </h2>
+            <TimelineHorizontal
+              events={[
+                {
+                  heading: "Prosjekt hos kunde",
+                  subHeading: "2016",
+                  direction: "top",
+                },
+                {
+                  heading: "Ansettelse hos Advania",
+                  subHeading: "2019",
+                  direction: "bottom",
+                },
+                {
+                  heading: "Leder i gruppen",
+                  subHeading: "2021",
+                  direction: "top",
+                },
+              ]}
+            />
+          </div>
+        </main>
+        <Footer />
+      </EmailFormProvider>
+    </div>
+  );
+};
+
+export default AboutPage;
--- components/about/Hero.tsx
+++ components/about/Hero.tsx
@@ -4,18 +4,28 @@ const Hero = () => {
   return (
     <>
       <div className="flex flex-col items-stretch min-h-96 sm:flex-row  py-10 px-10 gap-6 ">
-        <div className="basis-1/3  flex  justify-center items-center ">
-          <img src="CVbilde.jpeg" alt="Ronny Wang" className="min-h-70" />
+        <div className="basis-1/4  flex  justify-center items-center ">
+          <img src="meg.png" alt="Ronny Wang" className="min-h-50" />
         </div>
 
-        <div className="basis-2/3  flex flex-col justify-center ">
+        <div className="basis-3/4  flex flex-col justify-center ">
           <h2>Ronny Wang</h2>
           <p>
-            Med bakgrunn i det og det ser jeg muligheten for å videreformidle
-            opplært kunnskap om det og det. Med hovedvekt på disse og de
-            områdene så ser jeg muligheten for det og det Med bakgrunn i det og det ser jeg muligheten for å videreformidle
-            opplært kunnskap om det og det. Med hovedvekt på disse og de
-            områdene så ser jeg muligheten for det og det
+            Jeg har jobbet 30 år med salg og salgsledelse innenfor fagområdet
+            CRM, der jeg har solgt og levert CRM-løsninger til mer enn 200
+            selskaper. Jeg har som gründer bygget opp og solgt 2 lønnsomme CRM
+            selskaper der jeg selv i dag leder den ene enheten som en del av
+            Advania Norge. Jeg er lidenskapelig bruker av CRM-systemer og vet at
+            riktig bruk, sammen med en god plan og velvillige brukere, gir
+            bedriften bedre mulighet for å finne, vinne og beholde sine kunder.
+            De siste 15 år har jeg også vært gjesteforelester på
+            Hanldeslhøyskolen BI og Høyskolen Kristiania der jeg har undervist
+            studenter i moderne salg og markedsføringsprinsipper. De siste årene
+            har jeg tatt til meg AI for bruk innenfor salg og er brennende
+            engasjert i hvordan AI-verktøy kan øke både personlig produktivitet,
+            men også bli strategisk verktøy for virksomheter i ulike bransjer.
+            Som rådgiver er jeg opptatt av å gi kundene mest mulig verdi av sin
+            CRM-investering.
           </p>
         </div>
       </div>
